791fd0a3487b6f3534f9404806e7668b73cdd274
aziz-codes <azizisoffline@gmail.com>
2023-10-31 11:48:26 -0700

    using hooks to control resize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Store } from "@/context/Context";
 const Navbar = () => {
   const { setClicked } = Store();
   return (
-    <nav className="sticky top-0 bg-white flex border h-14  z-50 w-full items-center justify-between">
+    <nav className="sticky top-0 bg-white flex border h-14  z-49 w-full items-center justify-between">
       <div
         className="h-6 w-6 outline-none block cursor-pointer ml-6 group"
         onClick={() => setClicked((prev) => !prev)}
